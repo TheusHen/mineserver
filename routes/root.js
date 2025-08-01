c1075bb29baa03625a2463bdd00fed7c975891f8
I'm Matheus <codelong@proton.me>
2025-08-01 19:50:42 -0300

    using '.end()' to avoid timeout

--- routes/root.js
+++ routes/root.js
@@ -3,7 +3,7 @@
 const cors = require('../middlewares/cors')
 
 module.exports = function (app) {
-    app.get('/', cors({ public: true }), async (req, res) => {
-        await new Promise(() => {})
+    app.get('/', cors({ public: true }), (req, res) => {
+        res.status(204).end();
     })
 }
